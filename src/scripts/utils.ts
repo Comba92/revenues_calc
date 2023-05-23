931c6adf1a38b50d5315fc4e7ea8214579d6756e
Comba92 <ivanmarco.spinello@gmail.com>
2023-05-23 13:32:23 +0200

    BUGFIX - I used pop to get the last month for the average. What a retard

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,4 +1,4 @@
-export const printPrice = (number: number) => '$' + Intl.NumberFormat().format(number)
+export const printPrice = (number: number) => Intl.NumberFormat().format(number) + '$'
 export function averageRevenueOfYear(revenues: number[]) {
-  return Math.round(revenues.pop()! / 12)
+  return Math.round(revenues[11] / 12)
 }
