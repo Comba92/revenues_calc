f19eba269406d7d5c7e9aea81e4a961f2338d449
Comba92 <ivanmarco.spinello@gmail.com>
2023-08-24 12:18:03 +0200

    Added fixed costs field

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -9,8 +9,9 @@ export default function InputForm({ setRevenues, monthsView, setMonthsView }) {
     const price = Number(event.target.elements.price.value)
     const newUsers = Number(event.target.elements.newUsers.value)
     const lostUsers = Number(event.target.elements.lostUsers.value)
+    const fixedCosts = Number(event.target.elements.fixedCosts.value)
 
-    const rev = computeYearsRevenues(price, newUsers, lostUsers)
+    const rev = computeYearsRevenues(price, newUsers, lostUsers, fixedCosts)
     setRevenues( rev )
   }
 
@@ -26,6 +27,7 @@ export default function InputForm({ setRevenues, monthsView, setMonthsView }) {
           <p>What's the <strong>price</strong>? <InputField name="price" label="$"/></p>
           <p>How many <strong>new users</strong>? <InputField name="newUsers" label="users"/></p>
           <p>How many <strong>lost users</strong>? <InputField name="lostUsers" label="%"/></p>
+          <p>How much are your <strong>fixed costs</strong>? <InputField name="fixedCosts" label="$"/></p>
         </fieldset>
         <div className="flex flex-wrap justify-center">
           <button type="submit" className="text-center px-8 py-3 font-semibold border rounded border-gray-800 text-gray-800">Compute!</button>
--- src/scripts/calculation.ts
+++ src/scripts/calculation.ts
@@ -1,25 +1,27 @@
 const YEARS_TO_SHOW = 4
 
 export default function computeYearsRevenues(
-  price: number, newCostumers: number, churnedCostumers: number
+  price: number, newCostumers: number, churnedCostumers: number, fixedCosts: number
 ): number[][] {
   let res: number[] = []
 
   let currentCostumers: number = newCostumers
 
-  res.push(price * currentCostumers)
+  // First month
+  res.push(price * currentCostumers - fixedCosts)
+
   for(let i=1; i<12 * YEARS_TO_SHOW; ++i) {
     const churnedCount = Math.round(currentCostumers * (churnedCostumers / 100))
-    console.log(currentCostumers, churnedCount, (churnedCostumers / 100))
+    // console.log(currentCostumers, churnedCount, (churnedCostumers / 100))
     const month1 = price * currentCostumers
     const month2 = price * newCostumers
     const month3 = price * churnedCount
     // total : 100 = x : churnedPercetnage
 
-    res.push(res[i-1] + month1 + month2 - month3)
+    res.push(res[i-1] + month1 + month2 - month3 - fixedCosts)
     currentCostumers = currentCostumers + (newCostumers - churnedCount)
   }
-  console.log(res)
+  //console.log(res)
 
   let final: number[][] = []
   for(let i = 0; i<YEARS_TO_SHOW; ++i) {
