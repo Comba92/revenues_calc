10be55e98fd13406ff03b64f411098ea24a99e48
Comba92 <ivanmarco.spinello@gmail.com>
2023-08-24 12:23:21 +0200

    Added variable costs field

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -10,8 +10,9 @@ export default function InputForm({ setRevenues, monthsView, setMonthsView }) {
     const newUsers = Number(event.target.elements.newUsers.value)
     const lostUsers = Number(event.target.elements.lostUsers.value)
     const fixedCosts = Number(event.target.elements.fixedCosts.value)
+    const variableCosts = Number(event.target.elements.variableCosts.value)
 
-    const rev = computeYearsRevenues(price, newUsers, lostUsers, fixedCosts)
+    const rev = computeYearsRevenues(price, newUsers, lostUsers, fixedCosts, variableCosts)
     setRevenues( rev )
   }
 
@@ -27,7 +28,8 @@ export default function InputForm({ setRevenues, monthsView, setMonthsView }) {
           <p>What's the <strong>price</strong>? <InputField name="price" label="$"/></p>
           <p>How many <strong>new users</strong>? <InputField name="newUsers" label="users"/></p>
           <p>How many <strong>lost users</strong>? <InputField name="lostUsers" label="%"/></p>
-          <p>How much are your <strong>fixed costs</strong>? <InputField name="fixedCosts" label="$"/></p>
+          <p>What are your <strong>fixed costs</strong>? <InputField name="fixedCosts" label="$"/></p>
+          <p>What are your <strong>variable costs</strong> per costumer? <InputField name="variableCosts" label="$"/></p>
         </fieldset>
         <div className="flex flex-wrap justify-center">
           <button type="submit" className="text-center px-8 py-3 font-semibold border rounded border-gray-800 text-gray-800">Compute!</button>
--- src/scripts/calculation.ts
+++ src/scripts/calculation.ts
@@ -1,14 +1,14 @@
 const YEARS_TO_SHOW = 4
 
 export default function computeYearsRevenues(
-  price: number, newCostumers: number, churnedCostumers: number, fixedCosts: number
+  price: number, newCostumers: number, churnedCostumers: number, fixedCosts: number, variableCosts: number
 ): number[][] {
   let res: number[] = []
 
   let currentCostumers: number = newCostumers
 
   // First month
-  res.push(price * currentCostumers - fixedCosts)
+  res.push(price*currentCostumers - fixedCosts - variableCosts*currentCostumers)
 
   for(let i=1; i<12 * YEARS_TO_SHOW; ++i) {
     const churnedCount = Math.round(currentCostumers * (churnedCostumers / 100))
@@ -16,9 +16,10 @@ export default function computeYearsRevenues(
     const month1 = price * currentCostumers
     const month2 = price * newCostumers
     const month3 = price * churnedCount
+    const month4 = variableCosts * currentCostumers
     // total : 100 = x : churnedPercetnage
 
-    res.push(res[i-1] + month1 + month2 - month3 - fixedCosts)
+    res.push(res[i-1] + month1 + month2 - month3 - month4 - fixedCosts)
     currentCostumers = currentCostumers + (newCostumers - churnedCount)
   }
   //console.log(res)
